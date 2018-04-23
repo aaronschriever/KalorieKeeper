8d06283d0d447d86ff2ff6f918c4900a7648ebe4
Aaron <aaron.schriever@gmail.com>
2018-04-23 23:09:21 +0930

    add rounding to bmr calculation. Add calculations and changer handlers to bmr form. Add type checking tests to bmr.test

--- kalorie-keeper/src/component/BmrForm.js
+++ kalorie-keeper/src/component/BmrForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import calculateBMR from '../logic/calculateBMR';
 
 class BmrForm extends Component {
     constructor(props){
@@ -8,16 +8,35 @@ class BmrForm extends Component {
             genderSelected:'M',
             height:'',
             weight:'',
-            age:''
+            age:'',
+            bmr:''
         }
         this.handleGenderChange = this.handleGenderChange.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleBMRChange = this.handleBMRChange.bind(this);
     }
     handleGenderChange(changeEvent){
         this.setState({genderSelected: changeEvent.target.value});
     }
+    handleBMRChange(event){
+        const bmr = calculateBMR(this.state.weight, this.state.height, this.state.age, this.state.genderSelected);
+        alert("calculating bmr with weight:"+this.state.weight + " height: " + this.state.height + " age: " + this.state.age + " gender: " + this.state.genderSelected);
+        event.preventDefault();
+        this.setState({bmr:bmr});
+        alert(this.state.bmr);
+    }
+    handleInputChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+          });
+    }
 render(){
     return (
     <div className="bmr-form">
+    <form onSubmit={this.handleBMRChange}>
     <div>
         <label>
             <input type="radio" group="gender" value="M" checked={this.state.genderSelected==='M'} onChange={this.handleGenderChange}/>Male
@@ -25,16 +44,17 @@ render(){
     <label>
         <input type="radio" group="gender" value="F" checked={this.state.genderSelected==='F'} onChange={this.handleGenderChange}/>Female
     </label>
-    <br/><input type="number"/>
+    <br/><input name="height" type="number" onChange={this.handleInputChange}/>
     <label> height &#40;cm&#41; </label>
-    <br/><input type="number"/>
+    <br/><input name="weight"type="number" onChange={this.handleInputChange}/>
     <label> weight &#40;kg&#41; </label>
-    <br/><input type="number"/>
+    <br/><input name="age"type="number" onChange={this.handleInputChange}/>
     <label> age &#40;years&#41; </label>
     </div>
     <div>
     <input type="submit" value="SAVE" />
     </div>
+    </form>
     </div>
     
 );
--- kalorie-keeper/src/logic/bmr.test.js
+++ kalorie-keeper/src/logic/bmr.test.js
@@ -4,3 +4,15 @@ test('calculate male BMR:  height 182cm, weight: 85kg, age: 34, equals 1909', ()
 
     expect(calculateBMR(85,182,34,'M')).toBe(1909);
 });
+test('calculate female BMR:  height 182cm, weight: 85kg, age: 34, equals 1639', () =>{
+
+    expect(calculateBMR(85,182,34,'F')).toBe(1639);
+});
+test('send error data: result "error" ', () =>{
+
+    expect(calculateBMR(85,182,34,'W')).toBe("error");
+});
+test('send error (NaN) data: result "error incorrect data types" ', () =>{
+
+    expect(calculateBMR('E',182,34,'F')).toBe("error incorrect data types");
+});
